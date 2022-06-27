fac2125368bdf0ca8cb4ab79bb9c3d318c5af46c
sanchezj <jesus.sanchez@creamteam.de>
2022-06-27 15:19:49 +0200

    Add urls to upload images

--- plant_app/api/urls.py
+++ plant_app/api/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from plant_app.api.views import PlantAV
+from plant_app.api.views import PlantAV, PlantDetailAV, PlantImageDetailAV
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('list/', PlantAV.as_view(), name='plant-list'),
     path('create/', PlantAV.as_view(), name='plant-create'),
-    
+    path('<int:pk>/', PlantDetailAV.as_view(), name='plant-detail'),
+    path('<int:pk>/add-image/', PlantImageDetailAV.as_view(), name='plant-detail'),
 ]
--- plant_app/api/views.py
+++ plant_app/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from datetime import datetime, timedelta
 from plant_app.models import Plant
 from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser
 
 
 class PlantAV(APIView):
@@ -19,9 +20,9 @@ class PlantAV(APIView):
         return Response(serializer.data)
     
     def post(self, request):
-        serializer = PlantSerializer(data=request.data['plant'])
+        serializer = PlantSerializer(data=request.data)
         user = self.request.user
-        if Plant.objects.filter(code=request.data['plant']['code']):
+        if Plant.objects.filter(code=request.data['code']):
             return Response({'error': 'There is already a plant with that code'}, status=status.HTTP_400_BAD_REQUEST)
         else:
             if serializer.is_valid() :
@@ -33,4 +34,27 @@ class PlantAV(APIView):
                 return Response(serializer.data)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PlantDetailAV(APIView):
+    
+    def get(self, request, pk):
+        try:
+            plant = Plant.objects.get(pk=pk)
+        except Plant.DoesNotExist:
+            return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = PlantSerializer(plant, context={'request': request})
+        return Response(serializer.data)
+    
+class PlantImageDetailAV(APIView):
+    parser_class = (FileUploadParser,)
+    
+    def post(self, request, pk):
         
+        file_serializer = PlantImageSerializer(data=request.data)
+
+        if file_serializer.is_valid():
+            file_serializer.save()
+            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'error': file_serializer.errors},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
